abd8c061e54f9f5953f4821cc958461ddfa42c1d
Moussatech127 <tvvtez17@gmail.com>
2024-11-16 18:24:08 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "221771084463"
+global.ownername = "༒𝔒𝔟𝔦𝔱𝔬𖧷⃝⃝🌟𝔫𝔢𝔵𝔱𝔞𝔤𝔢༒"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
